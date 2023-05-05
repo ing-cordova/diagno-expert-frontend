fad7052bb36a7f9735ff19b34f3909f88a29d17c
Andres Cordova <andrescordovaoficial@gmail.com>
2023-05-04 21:29:57 -0600

    [ADD] Implementing the new login's design (not functionality yet) :sparkles:

--- diagno-expert/src/pages/auth.js
+++ diagno-expert/src/pages/auth.js
@@ -6,43 +6,49 @@ import styles from "./styles/auth.module.css";
 import AuthImage from "../assets/auth-image.svg";
 
 const AuthPage = () => {
+  const onSubmitHandler = () => {};
   return (
     <Layout>
       <MainContainer>
         <Container>
-          <div
-            style={{
-              background: "#2743AA",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: "55%",
-              borderRadius: "15px 0 0 15px",
-            }}
-          ></div>
-          <div
-            style={{
-              background: "white",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: "45%",
-              right: 0,
-              borderRadius: "0 15px 15px 0",
-            }}
-          >
-            <div className={styles["right-div"]}>
+          <div className={styles["div-left-container"]}>
+            <div className={styles["left-main-div"]}>
+              <h1 className={styles["h1-left"]}>DIAGNO EXPERT</h1>
+              <form onSubmit={onSubmitHandler}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className={styles["input-form"]}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className={styles["input-form"]}
+                />
+                <input
+                  type="button"
+                  value="SIGN IN"
+                  className={styles["button-send"]}
+                />
+              </form>
+              <p className={styles.guideline}>
+                -------------------------------------------------------
+              </p>
+              <p className={styles["p-create-account"]}>
+                Need to create an account?
+              </p>
+              <button className={styles['button-create-account']}>Create a free account now!</button>
+            </div>
+          </div>
+
+          <div className={styles["div-right-container"]}>
+            <div className={styles["right-main-div"]}>
               <h1 className={styles.h1}>Welcome back!</h1>
               <p className={styles.p}>
                 Login and start joining to the new diagnostics professional
                 team.
               </p>
-              <img
-                src={AuthImage}
-                alt="auth-login"
-                className={styles.img}
-              />
+              <img src={AuthImage} alt="auth-login" className={styles.img} />
             </div>
           </div>
         </Container>
